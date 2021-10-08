53c092b8e21d6157f079a9977d19fdb75711060c
Kartik Bhalla <kartikbhalla12@gmail.com>
2021-10-09 01:06:55 +0530

    GreetingController: add SayHello Get Request
    Signed-off-by: Kartik Bhalla <kartikbhalla12@gmail.com>

--- src/main/java/com/example/demospring/GreetingController.java
+++ src/main/java/com/example/demospring/GreetingController.java
@@ -15,4 +15,9 @@ public class GreetingController {
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
+
+    @GetMapping("/sayHello")
+    public SayHello sayHello(@RequestParam(value = "nick", defaultValue = "nick") String nick) {
+        return new SayHello(counter.incrementAndGet(), String.format(template, nick));
+    }
 }
